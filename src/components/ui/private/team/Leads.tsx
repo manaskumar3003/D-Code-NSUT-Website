5730450d03244851b378920c5a343b42474fc554
Manas Jain <manasdeepakjain@gmail.com>
2024-07-12 23:54:43 +0530

    Content and Picture Updated (Home and Team Page)

--- src/components/ui/private/team/Leads.tsx
+++ src/components/ui/private/team/Leads.tsx
@@ -8,7 +8,7 @@ function Lead() {
               <Card
                 name="Pranshu Lakhotia"
                 position="CP Lead"
-                img="https://mighty.tools/mockmind-api/content/human/22.jpg"
+                img="/core/PranshuLakhotia.jpeg"
                 social={[
                   { icon: <FaGithub />, link: "https://github.com" },
                   { icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/pranshu-lakhotia-41b201254?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAD6toRcBMRZ3d0oVSivrUfH4tROkyFR_viU&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3Bh9WeZja3SQuVgnK86RD6%2BQ%3D%3D" },
@@ -28,7 +28,7 @@ function Lead() {
               <Card
                 name="Aarushi Sinha"
                 position="Design Lead"
-                img="https://mighty.tools/mockmind-api/content/human/43.jpg"
+                img="/core/AarushiSinha.jpeg"
                 social={[
                   { icon: <FaGithub />, link: "https://github.com" },
                   { icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/aarushi-sinha-4a25a32a7?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAEnyrPoBqeX_D_J_DnlNWugd-OtHZTIvM74&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BDBCeuFo1R%2FGxS9%2BFv8c6Ow%3D%3D" },
@@ -38,7 +38,7 @@ function Lead() {
               <Card
                 name="Maithri Sti Meda"
                 position="Content Lead"
-                img="https://mighty.tools/mockmind-api/content/human/9.jpg"
+                img="/core/MaithriMeda.jpeg"
                 social={[
                   { icon: <FaGithub />, link: "https://github.com" },
                   { icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/maithri-meda/" },
@@ -48,7 +48,7 @@ function Lead() {
               <Card
                 name="Ashwajeet Bhatia"
                 position="Content Lead"
-                img="https://media.licdn.com/dms/image/D4D03AQHz8wT2g6NR5A/profile-displayphoto-shrink_100_100/0/1675617088965?e=1723075200&v=beta&t=QrbLkUgSqHO8ihy2AgskgLP5KAe77JYzHgtYEe91nuM"
+                img="/core/AshwajeetBhatia.jpg"
                 social={[
                   { icon: <FaGithub />, link: "https://github.com" },
                   { icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/ashwajeetbhatia/" },
@@ -58,17 +58,17 @@ function Lead() {
               <Card
                 name="Gaurav Sahu"
                 position="Outreach Lead"
-                img="https://media.licdn.com/dms/image/D4D03AQEGyZQo2YnSGA/profile-displayphoto-shrink_200_200/0/1700306410217?e=1723075200&v=beta&t=JC6MTKSWJ35Aq8A5O0jTGleQR1BfrLyTLKfJKXU5Gc0"
+                img="/core/GauravSahu.jpeg"
                 social={[
-                  { icon: <FaGithub />, link: "https://github.com" },
+                  { icon: <FaGithub />, link: "https://github.com/gauravsahuuu" },
                   { icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/gaurav-sahu-24369424b/" },
-                  { icon: <FaInstagram />, link: "https://instagram.com" },
+                  { icon: <FaInstagram />, link: "https://www.instagram.com/sahuu_gaurav/" },
                 ]}
               />
               <Card
                 name="Yashika"
                 position="Outreach Lead"
-                img="https://mighty.tools/mockmind-api/content/human/6.jpg"
+                img="/core/Yashika.jpg"
                 social={[
                   { icon: <FaGithub />, link: "https://github.com" },
                   { icon: <FaLinkedinIn />, link: "https://www.linkedin.com" },
@@ -96,7 +96,7 @@ function Card({
   return (
     <div>
       <div className="flex flex-col justify-center items-center text-center rounded-md 
-                      min-w-40 sm:min-w-44 lg:min-w-56 pb-3 md:pb-4 lg:pb-5 bg-[#1e1e1ed2] gap-3 text-[#F0F0F0]">
+                      min-w-40 sm:min-w-44 lg:min-w-56 pb-3 md:pb-4 lg:pb-5 bg-[#c2bfb4] gap-3 ">
         <img alt="Picture" className="w-20 h-20 -mt-10 sm:w-24 sm:h-24 sm:-mt-12 md:w-28 md:h-28 md:-mt-14 lg:w-32 lg:h-32 lg:-mt-16 rounded-full" src={img} />
           <div>
           <p className="lg:text-xl font-semibold">{name}</p>
--- src/components/ui/private/team/Presidents.tsx
+++ src/components/ui/private/team/Presidents.tsx
@@ -4,12 +4,12 @@ function President() {
       <div className="flex flex-col justify-center items-center gap-10 px-6 md:px-12 xl:px-32">
         <div className="text-center">
           <h2 className="text-center mb-4 text-2xl text-gray-900 font-bold md:text-4xl">Meet our Team</h2>
-          <p className="text-gray-600 lg:w-8/12 lg:mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus recusandae tempora sunt eveniet earum est praesentium omnis eos amet molestiae sint possimus, distinctio neque, obcaecati, ipsa architecto eum quae quidem dignissimos quod illum quia consequatur aliquid nulla. Voluptas reprehenderit dignissimos vel cumque quibusdam non ea?</p>
+          <p className="text-gray-600 lg:w-3/4 lg:mx-auto">Our team at D'Code NSUT comprises passionate and dedicated individuals committed to fostering a vibrant tech community. Each member brings unique skills and expertise, working collaboratively to organize events, workshops, and projects. Together, we strive to create an inclusive environment where members can learn, innovate, and excel. Meet the brilliant minds behind our initiatives, always ready to support and guide you on your journey in the tech world.</p>
         </div>
         <div className="flex flex-col sm:flex-row gap-12 lg:gap-16 items-center">
           <div className="space-y-4 text-center order-2 sm:order-1">
             <img className="w-64 h-64 mx-auto object-cover rounded-xl md:w-48 md:h-48 lg:w-64 lg:h-64"
-              src="https://mighty.tools/mockmind-api/content/human/38.jpg" alt="woman" width="1000" height="667" />
+              src="/core/YashiPitti.webp" alt="woman" width="1000" height="667" />
               <div>
               <a href="https://www.linkedin.com/in/yashi-pitti-19023423a?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAADt6hvwBewa_RLF6XjBzeKiXDhUFfY750hk&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BoxETLQsPTey6cdzlTm61Qg%3D%3D" target="_blank" className="text-2xl font-bold">Yashi Pitti</a>
               <span className="block text-sm text-gray-500">Vice-President</span>
@@ -25,7 +25,7 @@ function President() {
           </div>
           <div className="space-y-4 text-center order-2 sm:order-1">
             <img className="w-64 h-64 mx-auto object-cover rounded-xl md:w-48 md:h-48 lg:w-64 lg:h-64"
-              src="https://mighty.tools/mockmind-api/content/human/9.jpg" alt="woman" width="1000" height="667" />
+              src="/core/MuskanGupta.jpeg" alt="woman" width="1000" height="667" />
             <div>
             <a href="https://www.linkedin.com/in/muskan-gupta-96028b251?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAD4nv5gB5zgpVmHsBSHNuq8CfRAAXoqnk94&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BEqXEbFcXR%2Fma6wHqJfKFZg%3D%3D" target="_blank" className="text-2xl font-bold">Muskan Gupta</a>
               <span className="block text-sm text-gray-500">Vice-President</span>
